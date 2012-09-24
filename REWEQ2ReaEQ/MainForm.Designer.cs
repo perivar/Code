fbb22a32573163e04d3854f02adc3fe591665674
Per Ivar Nerseth <perivar@nerseth.com>
2012-09-24 21:15:20 +0200

    Added version number

--- REWEQ2ReaEQ/MainForm.Designer.cs
+++ REWEQ2ReaEQ/MainForm.Designer.cs
@@ -34,43 +34,48 @@
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.eqPlugin = new System.Windows.Forms.RadioButton();
 			this.label3 = new System.Windows.Forms.Label();
+			this.labelVersion = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(41, 106);
+			this.label1.Location = new System.Drawing.Point(55, 130);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(203, 61);
+			this.label1.Size = new System.Drawing.Size(271, 75);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Drag REW - Room EQ Wizard EQ Filter files here !";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(61, 158);
+			this.label2.Location = new System.Drawing.Point(81, 194);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(168, 48);
+			this.label2.Size = new System.Drawing.Size(224, 71);
 			this.label2.TabIndex = 1;
-			this.label2.Text = "Use either FBQ2496 or Generic Equaliser format. Remember to export filter as text" +
-			"!";
+			this.label2.Text = "Use either FBQ2496 or Generic Equaliser format. Remember to export Filter Setting" +
+			"s as text! [perivar@nerseth.com]";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(93, 7);
+			this.pictureBox1.Location = new System.Drawing.Point(135, 26);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(100, 96);
+			this.pictureBox1.Size = new System.Drawing.Size(102, 100);
 			this.pictureBox1.TabIndex = 2;
 			this.pictureBox1.TabStop = false;
 			// 
 			// eqPlugin
 			// 
-			this.eqPlugin.Location = new System.Drawing.Point(12, 237);
+			this.eqPlugin.Location = new System.Drawing.Point(16, 292);
+			this.eqPlugin.Margin = new System.Windows.Forms.Padding(4);
 			this.eqPlugin.Name = "eqPlugin";
-			this.eqPlugin.Size = new System.Drawing.Size(104, 24);
+			this.eqPlugin.Size = new System.Drawing.Size(139, 30);
 			this.eqPlugin.TabIndex = 3;
 			this.eqPlugin.TabStop = true;
 			this.eqPlugin.Text = "ReaEQ";
@@ -78,27 +83,39 @@
 			// 
 			// label3
 			// 
-			this.label3.Location = new System.Drawing.Point(12, 224);
+			this.label3.Location = new System.Drawing.Point(16, 276);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(100, 19);
+			this.label3.Size = new System.Drawing.Size(133, 23);
 			this.label3.TabIndex = 4;
 			this.label3.Text = "Choose EQ Plugin:";
 			// 
+			// labelVersion
+			// 
+			this.labelVersion.Location = new System.Drawing.Point(2, 0);
+			this.labelVersion.Name = "labelVersion";
+			this.labelVersion.Size = new System.Drawing.Size(365, 19);
+			this.labelVersion.TabIndex = 5;
+			this.labelVersion.Text = "Version: ";
+			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(379, 322);
+			this.Controls.Add(this.labelVersion);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.eqPlugin);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
+			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "MainForm";
 			this.Text = "REW EQ to VST EQ Preset";
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Label labelVersion;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.RadioButton eqPlugin;
 		private System.Windows.Forms.PictureBox pictureBox1;
